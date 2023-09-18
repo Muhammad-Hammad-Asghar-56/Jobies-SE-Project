e9cd1feaab0acc79d06a569608bd78a62d5f3831
Hasan Nawaz <98684715+hasannawaz2486@users.noreply.github.com>
2023-09-18 12:00:00 +0500

    client

---
+++ client/src/assets/index.js
@@ -0,0 +1,44 @@
+import NoProfile from "./userprofile.png";
+import HeroImage from "./hero.png";
+import Google from "./google.png";
+import Linkedin from "./linkedin.png";
+import Youtube from "./youtube.png";
+import WhatsApp from "./whatsapp.png";
+import Twitter from "./twitter.png";
+import Instagram from "./instagram.png";
+import Spotify from "./spotify.png";
+import Office from "./office.jpg";
+import JobImg from "./job.jpg";
+import Facebook from "./facebook.png";
+import CodeWave from "./codewave.png";
+//  import recently added photos
+//  by there properly names
+//  and export them
+import joblogo1 from "./joblogo1.png";
+import joblogo2 from "./joblogo2.png";
+import login from "./login.png";
+// register and sign up
+import register_ from "./register.png";
+import signup_ from "./signUp.jpg";
+
+export {
+  CodeWave,
+  NoProfile,
+  Facebook,
+  WhatsApp,
+  Twitter,
+  Instagram,
+  Spotify,
+  HeroImage,
+  Google,
+  Linkedin,
+  Youtube,
+  Office,
+  JobImg,
+  joblogo1,
+  joblogo2,
+  login,
+  register_,
+  signup_,
+
+};
